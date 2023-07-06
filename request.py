2c738710332c8aa868f06cc231b9fe180b28d302
Flo <dazflo@hotmail.fr>
2023-07-06 16:48:47 +0200

    Request improvments en typo fixes

--- request.py
+++ request.py
@@ -7,11 +7,11 @@ import lh_access as API
 
 
 # Initiate logging
-logger = logging.getlogger()
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 
-file_handler = logging.TimedRotatingFileHandler('logs/lh_requests.log', when='midnight', interval=1, backupCount=7)
+file_handler = logging.handlers.TimedRotatingFileHandler('logs/lh_requests.log', when='midnight', interval=1, backupCount=7)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -20,7 +20,7 @@ logger.addHandler(file_handler)
 headers = {"Authorization": API.BEARER}
 
 
-def execute_request(url):
+def get(url):
     """
     Execute request and get json object
     return data as json variable
@@ -28,11 +28,11 @@ def execute_request(url):
     try:
         req = requests.get(url, headers=headers)
         if req.status_code == 200:
-            logger.info(f"{req.status_code} url")
+            logger.info(f"{req.status_code} {url}")
             return req.json()
         else:
-            logger.error(f"{req.status_code} url")
-            logger.error(f"req.text")
+            logger.error(f"{req.status_code} {url}")
+            logger.error(f"{req.text}")
     except Exception as e:
             logger.error(f"Error when reaching {url} : {e}")
     return
